a24ecac97c2a4766dba4a37dfe78ab9e9803265e
ivobral <ivobral@hotmail.com>
2021-09-22 23:10:39 +0200

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+//za lakše importanje u App.js fajlu
+export { default as Navbar } from './Navbar';
+export { default as Cryptocurrencies } from './Cryptocurrencies';
+export { default as CryptoDetails } from './CryptoDetails';
+export { default as Exchanges } from './Exchanges';
+export { default as Homepage } from './Homepage';
+export { default as News } from './News';
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '702672d1a4msh1ec40d97221114ap13cf96jsn069ba531a87b'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
